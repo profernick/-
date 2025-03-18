7f3ec6f05bdea5b645bb4c9821f8a48027681d33
profernick <114310414+profernick@users.noreply.github.com>
2025-03-18 22:21:29 +0700

    Update collab.sh

--- collab.sh
+++ collab.sh
@@ -1,3 +1,4 @@
+echo Not my scripts im update a ngrok url
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
